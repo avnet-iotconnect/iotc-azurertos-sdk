bf89981a5e30ba88e82810f910f92b846163e61c
Nik Markovic <nik.markovic@vensi.com>
2021-05-17 15:07:05 -0500

    Prevent accidental commits of private information in device configuration files

--- scripts/setup-project.sh
+++ scripts/setup-project.sh
@@ -62,6 +62,15 @@ pushd "${project_dir}" >/dev/null
     echo cp -nr $dir ../../
     cp -nr $dir ../../
   done
+  # prevent accidental commit of private information
+  # export NO_ASSUME_UNCHANGED=yes to allow commits to these files
+  if [[ -n "$NO_ASSUME_UNCHANGED" ]]; then
+    git update-index --no-assume-unchanged basic-sample/src/sample_device_identity.c
+    git update-index --no-assume-unchanged basic-sample/include/app_config.h
+  else
+    git update-index --assume-unchanged basic-sample/src/sample_device_identity.c
+    git update-index --assume-unchanged basic-sample/include/app_config.h
+  fi
 popd >/dev/null
 
 rm -rf $(dirname "${project_dir}")
