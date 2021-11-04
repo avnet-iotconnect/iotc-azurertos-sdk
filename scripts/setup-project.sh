ecb86ece1c6d75613b24d1cee4687b7f07605c1a
Nik Markovic <nik.markovic@vensi.com>
2021-11-04 16:56:38 -0500

    mimxrt10xx-package: fix naming errors

--- scripts/setup-project.sh
+++ scripts/setup-project.sh
@@ -4,7 +4,7 @@ set -e
 
 name="${1}"
 if [[ -z "$name" ]]; then
-  echo "Usge: $0 <mimxrt1060|stm32l4|same54xpro|mimxrt10xx_package>"
+  echo "Usge: $0 <mimxrt1060|stm32l4|same54xpro|mimxrt10xx-package>"
   exit 1
 fi
 pushd "$(dirname $0)"/../samples/"${name}"
