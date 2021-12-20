ddb0533ad48a306af48b347f535075e7a03e1ecd
shuliu1314 <Shu.liu@vensi.com>
2021-12-20 02:37:25 -0600

    test

--- scripts/setup-project.sh
+++ scripts/setup-project.sh
@@ -20,21 +20,21 @@ case "$name" in
   nxpdemos)
     mkdir iotconnectdemo
 	mkdir iotc-azrtos-sdk
-    mv overlay/rt1060/iotconnectdemo/ iotconnectdemo/.
+    mv overlay/rt1060/iotconnectdemo/. iotconnectdemo/.
 	rm -rf overlay
     ;;
   maaxboardrt)
 	mkdir iotconnectdemo
 	mkdir board
 	mkdir iotc-azrtos-sdk
-    mv overlay/maaxboardrt/iotconnectdemo/ iotconnectdemo/.
-	mv overlay/maaxboardrt/board/ board/.
+    mv overlay/maaxboardrt/iotconnectdemo/. iotconnectdemo/.
+	mv overlay/maaxboardrt/board/. board/.
 	rm -rf overlay
     ;;
   lpc55s69)
 	mkdir iotconnectdemo
 	mkdir iotc-azrtos-sdk
-    mv overlay/lpc55s69/iotconnectdemo/ iotconnectdemo/.
+    mv overlay/lpc55s69/iotconnectdemo/. iotconnectdemo/.
 	rm -rf overlay
     ;;
 esac
