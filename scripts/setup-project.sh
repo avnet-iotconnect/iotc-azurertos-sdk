faba57ca1191e59065a16624a78030ac5f448af2
shuliu1314 <Shu.liu@vensi.com>
2022-09-01 12:58:51 -0500

    script test

--- scripts/setup-project.sh
+++ scripts/setup-project.sh
@@ -170,18 +170,12 @@ case "$name" in
     wget -q -O azrtos.zip https://saleshosted.z13.web.core.windows.net/sdk/AzureRTOS/Azure_RTOS_6.1_STM32L4+-DISCO_STM32CubeIDE_Samples_2021_11_03.zip
     project_dir='b-l4s5i-iot01a/stm32cubeide'
     libs="stm32l4xx_lib "
-	popd >/dev/null
-	echo Done
-    exit 0
     ;;
   mimxrt1060)
     echo Downloading Azure_RTOS_6...
     wget -q -O azrtos.zip https://saleshosted.z13.web.core.windows.net/sdk/AzureRTOS/Azure_RTOS_6.1_MIMXRT1060_MCUXpresso_Samples_2021_11_03.zip
     project_dir='mimxrt1060/MCUXpresso/'
     libs="mimxrt1060_library filex "
-	popd >/dev/null
-	echo Done
-    exit 0
     ;;
   same54xpro)
     echo Downloading Azure_RTOS_6...
