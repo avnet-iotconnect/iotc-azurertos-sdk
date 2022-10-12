560413bfb787c124a098e76aff8853082108fd61
shuliu1314 <Shu.liu@vensi.com>
2022-10-12 15:00:30 -0500

    Remove personal info in the config file

--- samples/mimxrt1060/basic-sample/include/app_config.h
+++ samples/mimxrt1060/basic-sample/include/app_config.h
@@ -6,11 +6,11 @@
 #define APP_CONFIG_H
 #define WIFI_SSID       ""
 #define WIFI_PASSWORD   ""
-#define IOTCONNECT_CPID "avtds"
-#define IOTCONNECT_ENV  "Avnet"
-#define IOTCONNECT_DUID "fae-rt1060" // you can supply a custom device UID, or...
+#define IOTCONNECT_CPID ""
+#define IOTCONNECT_ENV  ""
+//#define IOTCONNECT_DUID "custom ID" // you can supply a custom device UID, or...
 #define DUID_PREFIX "nxp-" // mac address will be appended in format 012345abcdef
 // if it is not defined, device certs will be used in iotconnect_app.c
-#define IOTCONNECT_SYMETRIC_KEY "c2h1aXNteWJlc3RmcmllbmQ="
+#define IOTCONNECT_SYMETRIC_KEY ""
 #define SAMPLE_SNTP_SERVER_NAME "time.google.com"    /* SNTP Server.  */
 #endif // APP_CONFIG_H
