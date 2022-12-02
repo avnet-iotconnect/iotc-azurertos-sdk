757fb87e289f6794048566212cda9dbfa44508d3
Russell Bateman <rbateman@witekio.com>
2022-12-02 12:41:10 +0000

    Refactor setup-project to use threadx symlinks

--- scripts/setup-project.sh
+++ scripts/setup-project.sh
@@ -22,6 +22,145 @@ get_file_name() {
   echo $1 | rev | cut -d '/' -f1 | rev
 }
 
+legacy_threadx_setup() {
+  name = $1
+  case "$name" in
+  maaxboardrt)
+  #symlink iotc-azrtos-sdk into project
+      echo Downloading MaaxXBoardRT baseline project packages...
+      wget -q -O azrtos.zip https://saleshosted.z13.web.core.windows.net/sdk/AzureRTOS/Azure_RTOS_6.1_MIMXRT1060_MCUXpresso_Samples_2021_11_03.zip
+      azrtos_dir='mimxrt1060/MCUXpresso/'
+      wget -q -O project.zip https://saleshosted.z13.web.core.windows.net/sdk/AzureRTOS/evkmimxrt1170_azure_iot_embedded_sdk.zip
+      project_dir=evkmimxrt1170_azure_iot_embedded_sdk/
+
+      echo Extracting...
+      unzip -q azrtos.zip
+      unzip -q project.zip
+
+      rm -rf ${project_dir}/azure-rtos/binary/netxduo
+      rm -rf ${project_dir}/azure-rtos/netxduo
+      rm -rf ${project_dir}/azure_iot
+      rm -rf ${project_dir}/drivers
+
+      rm -f azrtos.zip
+      rm -f project.zip
+
+      #copy original NXP project and netxduo lib into project
+      cp -nr ${project_dir}/* basic-sample
+      cp -nr ${azrtos_dir}/netxduo/* netxduo
+
+      rm -rf $(dirname "${azrtos_dir}")
+      rm -rf ${project_dir}
+
+      ;;
+    stm32l4)
+      echo Downloading Azure_RTOS_6...
+      wget -q -O azrtos.zip https://saleshosted.z13.web.core.windows.net/sdk/AzureRTOS/Azure_RTOS_6.1_STM32L4+-DISCO_STM32CubeIDE_Samples_2021_11_03.zip
+      project_target_dir='b-l4s5i-iot01a/'
+      project_ide_dir='stm32cubeide/'
+      libs="stm32l4xx_lib common_hardware_code "
+      ;;
+    mimxrt1060)
+      echo Downloading Azure_RTOS_6...
+      wget -q -O azrtos.zip https://saleshosted.z13.web.core.windows.net/sdk/AzureRTOS/Azure_RTOS_6.1_MIMXRT1060_MCUXpresso_Samples_2021_11_03.zip
+      project_target_dir='mimxrt1060/'
+      project_ide_dir='MCUXpresso/'
+      libs="mimxrt1060_library filex common_hardware_code "
+      ;;
+    same54xpro)
+      echo Downloading Azure_RTOS_6...
+      #wget -q -O azrtos.zip https://github.com/azure-rtos/samples/releases/download/v6.1_rel/Azure_RTOS_6.1_ATSAME54-XPRO_MPLab_Samples_2020_10_10.zip
+      #wget -q -O azrtos.zip https://saleshosted.z13.web.core.windows.net/sdk/AzureRTOS/Azure_RTOS_6.1_ADU_ATSAME54-XPRO_MPLab_Sample_2021_03_02.zip
+      #wget -q -O azrtos.zip https://github.com/azure-rtos/samples/releases/download/v6.1_rel/Azure_RTOS_6.1_ATSAME54-XPRO_MPLab_Samples_2021_11_03.zip
+      wget -q -O azrtos.zip https://saleshosted.z13.web.core.windows.net/sdk/AzureRTOS/Azure_RTOS_6.1_ATSAME54-XPRO_MPLab_Samples_2021_11_03.zip
+      project_target_dir='same54Xpro/'
+      project_ide_dir='mplab/'
+      libs="same54_lib filex common_hardware_code "
+      ;;
+    rx65ncloudkit)
+      echo Downloading Azure_RTOS_6...
+      wget -q -O azrtos.zip https://github.com/azure-rtos/samples/releases/download/v6.1_rel/Azure_RTOS_6.1_RX65N_Cloud_Kit_E2Studio_GNURX_Samples_2022_05_25.zip
+      project_target_dir=''
+      project_ide_dir='e2studio_gnurx/'
+      libs="filex netxduo_addons "
+      ;;
+    *)
+      popd >/dev/null
+      echo Done
+      exit 0
+      ;;
+  esac
+
+  project_dir="${project_target_dir}${project_ide_dir}"
+
+  case "$name" in
+    mimxrt1060 | stm32l4 | same54xpro | rx65ncloudkit)
+      echo Extracting...
+      unzip -q azrtos.zip
+      rm -f azrtos.zip
+      ;;
+  esac
+
+
+  # copy  only relevant directories into corresponding locations without overwriting
+  pushd "${project_dir}" >/dev/null
+    for dir in threadx netxduo $libs; do
+      echo cp -nr $dir $sample_dir
+      cp -nr $dir $sample_dir
+  #	rsync -av --exclude 'nbproject' $dir ../../
+    done
+  popd >/dev/null
+
+  rm -rf ${project_dir}
+
+    echo 'Applying patches for AzureRTOS component directory name references...'
+  case "$name" in
+    stm32l4)
+  echo 'Applying patches for AzureRTOS component directory name references...'
+      sed -i '/stm32l475_lib/d' ./stm32l4xx_lib/.cproject
+      sed -i 's#nxd#netxduo#g' ./netxduo/.cproject
+      sed -i 's#tx#threadx#g' ./threadx/.cproject
+      ;;
+    mimxrt1060 | rx65ncloudkit)
+      echo 'Applying patches for AzureRTOS component directory name references...'
+      sed -i 's#nxd#netxduo#g' ./netxduo/.cproject
+      sed -i 's#tx#threadx#g' ./threadx/.cproject
+      ;;
+  esac
+}
+
+create_threadx_sym_links() {
+  pushd threadx/ >/dev/null
+    for f in ../../../os/threadx/*; do
+      make_sym_link $f $(get_file_name $f)
+    done
+  popd >/dev/null
+
+  pushd netxduo/ >/dev/null
+    for f in ../../../os/netxduo/*; do
+      make_sym_link $f $(get_file_name $f)
+    done
+  popd >/dev/null
+
+  pushd filex/ >/dev/null
+    for f in ../../../os/filex/*; do
+      make_sym_link $f $(get_file_name $f)
+    done
+  popd >/dev/null
+}
+
+# prevent accidental commit of private information by default
+# export NO_ASSUME_UNCHANGED=yes to allow commits to these files
+git_hide_config_files() {
+  if [[ -n "$NO_ASSUME_UNCHANGED" ]]; then
+      git update-index --no-assume-unchanged basic-sample/src/sample_device_identity.c
+      git update-index --no-assume-unchanged basic-sample/include/app_config.h
+  else
+    git update-index --assume-unchanged basic-sample/src/sample_device_identity.c
+    git update-index --assume-unchanged basic-sample/include/app_config.h
+  fi
+}
+
 name="${1}"
 if [[ -z "$name" ]]; then
   echo "Usage: $0 <project_name>"
@@ -56,7 +195,6 @@ sample_dir=$(pwd)
 # initial cleanup
 
 rm -rf iotc-c-lib cJSON libTO b-l4s5i-iot01a mimxrt1060 same54Xpro stm32l4 maaxboardrt rx65ncloudkit
-
 
 case "$name" in
   same54xprov2)
@@ -111,16 +249,7 @@ case "$name" in
     rm -rf $(dirname "${azrtos_dir}")
     rm -rf ${project_dir}
 
-    # prevent accidental commit of private information by default
-    # export NO_ASSUME_UNCHANGED=yes to allow commits to these files
-    if [[ -n "$NO_ASSUME_UNCHANGED" ]]; then
-      git update-index --no-assume-unchanged basic-sample/src/sample_device_identity.c
-        git update-index --no-assume-unchanged basic-sample/include/app_config.h
-    else
-      git update-index --assume-unchanged basic-sample/src/sample_device_identity.c
-        git update-index --assume-unchanged basic-sample/include/app_config.h
-    fi
-
+    git_hide_config_files
     ;;
   stm32l4 | mimxrt1060 | same54xpro | rx65ncloudkit)
     pushd iotc-azrtos-sdk/ >/dev/null
@@ -128,15 +257,7 @@ case "$name" in
         make_sym_link $f $(get_file_name $f)
       done
     popd >/dev/null
-    # prevent accidental commit of private information by default
-    # export NO_ASSUME_UNCHANGED=yes to allow commits to these files
-    if [[ -n "$NO_ASSUME_UNCHANGED" ]]; then
-      git update-index --no-assume-unchanged basic-sample/src/sample_device_identity.c
-      git update-index --no-assume-unchanged basic-sample/include/app_config.h
-    else
-      git update-index --assume-unchanged basic-sample/src/sample_device_identity.c
-      git update-index --assume-unchanged basic-sample/include/app_config.h
-    fi
+    git_hide_config_files
     ;;
   *)
     echo "Unknown project name $name"
@@ -144,78 +265,16 @@ case "$name" in
 esac
 
 case "$name" in
-  stm32l4)
-    echo Downloading Azure_RTOS_6...
-    wget -q -O azrtos.zip https://saleshosted.z13.web.core.windows.net/sdk/AzureRTOS/Azure_RTOS_6.1_STM32L4+-DISCO_STM32CubeIDE_Samples_2021_11_03.zip
-    project_target_dir='b-l4s5i-iot01a/'
-    project_ide_dir='stm32cubeide/'
-    libs="stm32l4xx_lib common_hardware_code "
-    ;;
-  mimxrt1060)
-    echo Downloading Azure_RTOS_6...
-    wget -q -O azrtos.zip https://saleshosted.z13.web.core.windows.net/sdk/AzureRTOS/Azure_RTOS_6.1_MIMXRT1060_MCUXpresso_Samples_2021_11_03.zip
-    project_target_dir='mimxrt1060/'
-    project_ide_dir='MCUXpresso/'
-    libs="mimxrt1060_library filex common_hardware_code "
-    ;;
-  same54xpro)
-    echo Downloading Azure_RTOS_6...
-    #wget -q -O azrtos.zip https://github.com/azure-rtos/samples/releases/download/v6.1_rel/Azure_RTOS_6.1_ATSAME54-XPRO_MPLab_Samples_2020_10_10.zip
-    #wget -q -O azrtos.zip https://saleshosted.z13.web.core.windows.net/sdk/AzureRTOS/Azure_RTOS_6.1_ADU_ATSAME54-XPRO_MPLab_Sample_2021_03_02.zip
-	  #wget -q -O azrtos.zip https://github.com/azure-rtos/samples/releases/download/v6.1_rel/Azure_RTOS_6.1_ATSAME54-XPRO_MPLab_Samples_2021_11_03.zip
-	  wget -q -O azrtos.zip https://saleshosted.z13.web.core.windows.net/sdk/AzureRTOS/Azure_RTOS_6.1_ATSAME54-XPRO_MPLab_Samples_2021_11_03.zip
-    project_target_dir='same54Xpro/'
-    project_ide_dir='mplab/'
-    libs="same54_lib filex common_hardware_code "
+  stm32l4 | mimxrt1060 | same54xpro)
+    legacy_threadx_setup $name
     ;;
   rx65ncloudkit)
-    echo Downloading Azure_RTOS_6...
-	  wget -q -O azrtos.zip https://saleshosted.z13.web.core.windows.net/sdk/AzureRTOS/Azure_RTOS_6.1_RX65N_Cloud_Kit_E2Studio_GNURX_Samples_2022_05_25.zip
-    project_target_dir=''
-    project_ide_dir='e2studio_gnurx/'
-    libs="filex netxduo_addons "
+    create_threadx_sym_links
     ;;
   *)
     popd >/dev/null
   	echo Done
     exit 0
-    ;;
-esac
-
-project_dir="${project_target_dir}${project_ide_dir}"
-
-case "$name" in
-  mimxrt1060 | stm32l4 | same54xpro | rx65ncloudkit)
-    echo Extracting...
-    unzip -q azrtos.zip
-    rm -f azrtos.zip
-    ;;
-esac
-
-
-# copy  only relevant directories into corresponding locations without overwriting
-pushd "${project_dir}" >/dev/null
-  for dir in threadx netxduo $libs; do
-    echo cp -nr $dir $sample_dir
-    cp -nr $dir $sample_dir
-#	rsync -av --exclude 'nbproject' $dir ../../
-  done
-popd >/dev/null
-
-rm -rf ${project_dir}
-
-  echo 'Applying patches for AzureRTOS component directory name references...'
-case "$name" in
-  stm32l4)
-echo 'Applying patches for AzureRTOS component directory name references...'
-    sed -i '/stm32l475_lib/d' ./stm32l4xx_lib/.cproject
-    sed -i 's#nxd#netxduo#g' ./netxduo/.cproject
-    sed -i 's#tx#threadx#g' ./threadx/.cproject
-    ;;
-  mimxrt1060 | rx65ncloudkit)
-    echo 'Applying patches for AzureRTOS component directory name references...'
-    sed -i 's#nxd#netxduo#g' ./netxduo/.cproject
-    sed -i 's#tx#threadx#g' ./threadx/.cproject
     ;;
 esac
 
