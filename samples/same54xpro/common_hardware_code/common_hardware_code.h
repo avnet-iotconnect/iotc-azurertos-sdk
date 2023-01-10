969738c55a459ef254df505441fc14d72c200556
Shu Liu <liushu1314@gmail.com>
2023-01-10 16:27:07 -0600

    Restore SAME54 ADU support (#24)
    * add ADU support to SAME54 board
    
    * Update the sdk from 2021_03_02 to 2022_04_10 AzureRTOS 6.1
    
    * script and readme updates.
    
    Co-authored-by: shuliu1314 <Shu.liu@vensi.com>

--- samples/same54xpro/common_hardware_code/common_hardware_code.h
+++ samples/same54xpro/common_hardware_code/common_hardware_code.h
@@ -2,10 +2,6 @@
 #define COMMON_HARDWARE_CODE_HEADER
 
 #include "tx_api.h"
-#include "stdio.h"
-
-#define COUNTERS 5000
-extern const char output_string[];
 
 /* Define prototypes. */
 VOID board_setup();
