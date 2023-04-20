96f83b58fb434d94635a6722aac9daf8f5a53e36
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2023-04-20 13:04:19 -0600

    Update temphum14.c
    Syntax and logic fixes
    
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- samples/wfi32iot/src/clicks/temphum14.c
+++ samples/wfi32iot/src/clicks/temphum14.c
@@ -4,18 +4,18 @@ extern APP_SENSORS_DATA APP_SENSORS_data;
 
 //This function resets the sensor and pulls down
 //its serial number to prove that it is connected
-uint32_t TEMPHUM14_init ( uint8_t addr )
+uint32_t TEMPHUM14_init (uint8_t addr)
 {
-    APP_SENSORS_writeByte(TEMPHUM14_I2C_SLAVE_ADDR_GND, TEMPHUM14_CMD_RESET);
+    APP_SENSORS_writeByte(addr, TEMPHUM14_CMD_RESET);
     tx_thread_sleep(20);
     for (int index = 0; index < TEMPHUM14_SERIAL_NUMBER_BYTES; index++) {
         APP_SENSORS_data.i2c.rxBuffBytes[index] = 0;
     }   
-    return (TEMPHUM14_getSerialNumber(TEMPHUM14_I2C_SLAVE_ADDR_GND));
+    return (TEMPHUM14_getSerialNumber(addr));
 }
 //This function triggers a single temperature and 
 //humidity conversion with selected resolution
-void TEMPHUM14_setConversion ( uint8_t addr, uint8_t hum_osr, uint8_t temp_osr )
+void TEMPHUM14_setConversion (uint8_t addr, uint8_t hum_osr, uint8_t temp_osr)
 {
     uint8_t tmp;
     
@@ -32,7 +32,7 @@ void TEMPHUM14_setConversion ( uint8_t addr, uint8_t hum_osr, uint8_t temp_osr )
 //This function reads the temperature and humidity
 //data from the sensor and uses calibration values
 //to scale the data and update the data array parameter
-void TEMPHUM14_getTemperatureHumidity ( uint8_t addr, temphum14_data_struct *temphum14_data)
+void TEMPHUM14_getTemperatureHumidity (uint8_t addr, temphum14_data_struct *temphum14_data)
 {
     //Clear the i2c buffer
     for (int index = 0; index < 10; index++) {
@@ -40,44 +40,40 @@ void TEMPHUM14_getTemperatureHumidity ( uint8_t addr, temphum14_data_struct *tem
     }
 
     //Send command to read the temperature and relative humidity
-    APP_SENSORS_writeReadBytes( addr, TEMPHUM14_CMD_READ_T_AND_RH, 6 );
+    APP_SENSORS_writeReadBytes(addr, TEMPHUM14_CMD_READ_T_AND_RH, 6);
     
     //Scale the temperature data
-    uint16_t raw_temperature = APP_SENSORS_data.i2c.rxBuffBytes[ 0 ];
+    uint16_t raw_temperature = APP_SENSORS_data.i2c.rxBuffBytes[0];
     raw_temperature <<= 8;
-    raw_temperature |= APP_SENSORS_data.i2c.rxBuffBytes[ 1 ];
-    float corrected_temperature = ( float ) raw_temperature;
+    raw_temperature |= APP_SENSORS_data.i2c.rxBuffBytes[1];
+    float corrected_temperature = (float) raw_temperature;
     corrected_temperature /= TEMPHUM14_CALC_16_BIT_DIVIDER;
     corrected_temperature *= TEMPHUM14_CALC_TEMP_MULTI_FACT;
     corrected_temperature -= TEMPHUM14_CALC_TEMP_SUB_FACT;
     temphum14_data->temperature = corrected_temperature;
     
     //Scale the humidity data
-    uint16_t raw_humidity = APP_SENSORS_data.i2c.rxBuffBytes[ 3 ];
+    uint16_t raw_humidity = APP_SENSORS_data.i2c.rxBuffBytes[3];
     raw_humidity <<= 8;
-    raw_humidity |= APP_SENSORS_data.i2c.rxBuffBytes[ 4 ];
-    float corrected_humidity = ( float ) raw_humidity;
+    raw_humidity |= APP_SENSORS_data.i2c.rxBuffBytes[4];
+    float corrected_humidity = (float) raw_humidity;
     corrected_humidity /= TEMPHUM14_CALC_16_BIT_DIVIDER;
     corrected_humidity *= TEMPHUM14_CALC_HUM_MULTI_FACT;
     temphum14_data->humidity = corrected_humidity;
 }
 
 //This function simply retrieves the serial number of the sensor
-uint32_t TEMPHUM14_getSerialNumber ( uint8_t addr )
+uint32_t TEMPHUM14_getSerialNumber (uint8_t addr)
 {
-    uint8_t rx_buf[ TEMPHUM14_SERIAL_NUMBER_BYTES ];
     uint32_t ser_numb;
 
     APP_SENSORS_writeReadBytes( addr, TEMPHUM14_CMD_READ_SERIAL_NUMBER, TEMPHUM14_SERIAL_NUMBER_BYTES );
-    for (int index = 0; index < TEMPHUM14_SERIAL_NUMBER_BYTES; index++) {
-        rx_buf[ index ] = APP_SENSORS_data.i2c.rxBuffBytes[ index ];
-    }
     
-    ser_numb = APP_SENSORS_data.i2c.rxBuffBytes[ 0 ];
+    ser_numb = APP_SENSORS_data.i2c.rxBuffBytes[0];
     ser_numb <<= 8;
-    ser_numb |= APP_SENSORS_data.i2c.rxBuffBytes[ 1 ];
+    ser_numb |= APP_SENSORS_data.i2c.rxBuffBytes[1];
     ser_numb <<= 8;
-    ser_numb |= APP_SENSORS_data.i2c.rxBuffBytes[ 2 ];
+    ser_numb |= APP_SENSORS_data.i2c.rxBuffBytes[2];
     
     return ser_numb;
 }
