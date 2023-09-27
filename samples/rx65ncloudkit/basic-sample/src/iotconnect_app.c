e4d12145497b399ece9b71e8b3bfe2ee092fc4b6
Alen Karnil <138698161+akarnil@users.noreply.github.com>
2023-09-27 16:00:04 +0100

    add led and echo support (#68)
    * add led and echo support
    
    * fix COMMAND_UNKNOWN typo
    
    * FREE() macro comment
    
    * does_command_match turned to function
    
    * get_command_type refactor
    
    * get rid of macro
    
    * fixed const char warning
    
    * reimplement const

--- samples/rx65ncloudkit/basic-sample/src/iotconnect_app.c
+++ samples/rx65ncloudkit/basic-sample/src/iotconnect_app.c
@@ -42,8 +42,31 @@ void memory_test() {
 #endif /* MEMORY_TEST */
 
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-function"
+
+// On some hardware tested, the behaviour of free() causes crashes if trying to free a null pointer
+// the macro avoids this and also sets the pointer to null afterwards
+#define FREE(x) if ((x)) { free((void*)x); (x) = NULL; }
+
+
+typedef enum command_type
+{
+    COMMAND_UNKNOWN = 0,
+    ECHO,
+    LED,
+    COMMAND_MAX
+} command_type_t;
+
+const char *command_strings[] = {
+    [ECHO] = "echo ",
+    [LED] = "led ",
+};
+
+static bool does_command_match(const char* input_str, const command_type_t command_enum)
+{
+    return (strncmp((input_str), command_strings[(command_enum)], strlen(command_strings[(command_enum)])) == 0);
+}
+
+
 static char* compose_device_id() {
 #define PREFIX_LEN (sizeof(DUID_PREFIX) - 1)
     uint8_t mac_addr[6] = { 0 };
@@ -54,7 +77,6 @@ static char* compose_device_id() {
     printf("DUID: %s\r\n", duid);
     return duid;
 }
-#pragma GCC diagnostic pop
 
 static void on_ota(IotclEventData data) {
     const char *message = NULL;
@@ -69,33 +91,92 @@ static void on_ota(IotclEventData data) {
     }
 }
 
-static void command_status(IotclEventData data, bool status, const char *command_name, const char *message) {
-    const char *ack = iotcl_create_ack_string_and_destroy_event(data, status, message);
-    printf("command: %s status=%s: %s\r\n", command_name, status ? "OK" : "Failed", message);
-    printf("Sent CMD ack: %s\r\n", ack);
-    iotconnect_sdk_send_packet(ack);
-    free((void*) ack);
+static void publish_message(const char* key_str,const char* value_str) {
+    IotclMessageHandle msg = iotcl_telemetry_create();
+    iotcl_telemetry_add_with_iso_time(msg, iotcl_iso_timestamp_now());
+    iotcl_telemetry_set_string(msg, key_str, value_str);
+
+    const char *str = iotcl_create_serialized_string(msg, false);
+    iotcl_telemetry_destroy(msg);
+    printf("Sending: %s\r\n", str);
+    iotconnect_sdk_send_packet(str); // underlying code will report an error
+    iotcl_destroy_serialized(str);
 }
 
-static void on_command(IotclEventData data) {
-	char* context = NULL;
-    char *command = iotcl_clone_command(data);
-    if (NULL != command) {
-    	char* token = strtok_r(command, " ", &context);
-    	if(!strncmp(token, "led", 3)) {
-    		token = strtok_r(NULL, " ", &context);
-    		RX65N_LED_STATE state = (token[0] == '1') ||
-    								(strncmp(token, "true", 4) == 0 ) ?
-    								ON : OFF;
-    		set_led(LED2, state);
-    		command_status(data, true, command, "LED set");
-    	} else {
-    		command_status(data, false, command, "Not implemented");
-    	}
-        free((void*) command);
-    } else {
-        command_status(data, false, "?", "Internal error");
+static command_type_t get_command_type(const char* command_str) {
+
+    for (command_type_t i = COMMAND_UNKNOWN; i < COMMAND_MAX; i++)
+    {
+        if (does_command_match(command_str, i))
+        {
+            return i;
+        }
     }
+
+    printf("Unknown command\r\n");
+    return COMMAND_UNKNOWN;
+}
+
+static bool command_led(const char* command_str){
+
+    if (!command_str)
+    {
+        return false;
+    }
+
+    const char* setting_ptr = command_str + strlen(command_strings[LED]);
+    RX65N_LED_STATE state = (setting_ptr[0] == '1') || (strncmp(setting_ptr, "true", sizeof("true")) == 0 ) ? ON : OFF;
+    set_led(LED2, state);
+    return true;    
+}
+
+// allocates data
+static void iotc_create_and_send_ack(IotclEventData data, bool success)
+{
+    const char* message = success ? "OK" : "Failed_or_not_implemented";
+
+    const char *ack = iotcl_create_ack_string_and_destroy_event(data, success, message);
+    printf("Sent CMD ack: %s\r\n", ack);
+    iotconnect_sdk_send_packet(ack);
+    FREE(ack);
+}
+
+static void command_status(IotclEventData data, const char *command_name) {
+
+    bool success = true; // set false by default case
+    int command_type = get_command_type(command_name);
+    switch (command_type){
+    case ECHO:
+        printf("%s\r\n", &command_name[strlen(command_strings[ECHO])]);
+        break;
+    case LED:
+        if (!command_led(command_name)){
+            success = false;
+        }
+        break;
+    default:
+        success = false;
+        printf("Unsupported command\r\n");
+        break;
+    }
+
+    publish_message("last_command", command_name);
+    iotc_create_and_send_ack(data,success);
+}
+
+
+static void on_command(IotclEventData data) {
+    char *command = iotcl_clone_command(data);
+    if(!command)
+    {
+        iotc_create_and_send_ack(data, false);
+        FREE(command);
+        return;
+    }
+
+    command_status(data, command);
+    FREE(command);
+
 }
 
 static void on_connection_status(IotConnectConnectionStatus status) {
